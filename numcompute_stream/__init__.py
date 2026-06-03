2b271dbccfafe366dcbb0ad32de46533028bc9f5
prateekbadsara <pbadsara@rediffmail.com>
2026-06-04 04:09:27 +0930

    Modified init to include the latest classes, functions

--- numcompute_stream/__init__.py
+++ numcompute_stream/__init__.py
@@ -4,12 +4,16 @@ from __future__ import annotations
 from .io import load_csv, stream_csv, iter_chunks, save_csv
 from .stats import StreamingStats, StreamingHistogram, P2Quantile
 from .preprocessing import StandardScaler, Imputer, OneHotEncoder
+from .tree import DecisionTreeClassifier, HoeffdingTreeClassifier
+from .ensemble import EnsembleClassifier, RandomForestClassifier
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 __all__ = [
     "load_csv", "stream_csv", "iter_chunks", "save_csv",
     "StreamingStats", "StreamingHistogram", "P2Quantile",
     "StandardScaler", "Imputer", "OneHotEncoder",
+    "DecisionTreeClassifier", "HoeffdingTreeClassifier",
+    "EnsembleClassifier", "RandomForestClassifier",
     "__version__",
 ]
